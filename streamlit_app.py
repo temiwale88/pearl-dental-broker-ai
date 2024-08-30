74236a204a5b03f2c49d6d353a065a333fb8b862
Elijah Adeoye <temiwale88@gmail.com>
2024-08-30 07:59:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -645,20 +645,12 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-hide_github_icon_v2 = """
-    #MainMenu {
-      visibility: hidden;
-    }
-"""
-
 hide_github_icon = """
 #GithubIcon {
   visibility: hidden;
 }
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
-st.markdown(hide_github_icon_v2, unsafe_allow_html=True)
-
 
 st.sidebar.write(f"""
     ### Description
