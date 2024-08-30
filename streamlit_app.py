10de92bd4843e69b139f530eddb7b7bc9a33908b
Elijah Adeoye <temiwale88@gmail.com>
2024-08-30 08:02:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -632,18 +632,12 @@ if prompt := st.chat_input("Let's chat about affordable dental plans!"):
     st.session_state.messages.append({"role": "assistant", "content": response})
 # print(st.session_state)
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_github_icon = """
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, 
+.viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 st.sidebar.write(f"""
     ### Description
